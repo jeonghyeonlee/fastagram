545766a2fb576758fa3f986fa1f6ef65ced78f1f
jenghyeonlee <sodarain730@gmail.com>
2016-03-29 15:58:19 +0900

    init HomeView: render template home.html

--- fastagram/fastagram/urls.py
+++ fastagram/fastagram/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
-
 from users.views import *
 
+from fastagram.fastagram.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', HomeView.as_view(), name='home'),
+
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', LogoutView.as_view(), name='logout'),
     url(r'^signup/$', SignupView.as_view(), name='signup'),
