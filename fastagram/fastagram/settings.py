feab3d63ce881db1818d93cc00dc9beac9d83f07
jenghyeonlee <sodarain730@gmail.com>
2016-03-31 16:25:57 +0900

    refactoring settings: use dj_database_url -> edit settings.py and .env

--- fastagram/fastagram/settings.py
+++ fastagram/fastagram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,10 +85,9 @@ WSGI_APPLICATION = 'fastagram.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': dj_database_url.config(
+       default=os.environ.get("DATABASE_URL")
+   )
 }
 
 
