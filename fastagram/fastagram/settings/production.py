e27f9c7a90ccc5d3a7b25eb3a1ece2ef9f25b9a6
jenghyeonlee <sodarain730@gmail.com>
2016-04-01 07:24:56 +0900

    integrate getsentry: error monitoring

--- fastagram/fastagram/settings/production.py
+++ fastagram/fastagram/settings/production.py
@@ -1,7 +1,20 @@
+import raven
+
 from .partials import *
 
 
 DEBUG = False
 
 ALLOWED_HOSTS = [
+    "*",
 ]
+
+
+INSTALLED_APPS += [
+    'raven.contrib.django.raven_compat',
+]
+
+
+RAVEN_CONFIG = {
+    'dsn': 'https://e456beea56bc47489828c0c37a8e5be0:0bb0394775b14f419953e9ecb670a6a7@app.getsentry.com/72478',
+}
